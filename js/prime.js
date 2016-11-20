a72f5821d01816c7b6bad9d8dcccfd5c721023a7
melyaman <mohamed.elyamani@telecom-bretagne.eu>
2016-11-20 18:03:12 +0100

    modified prime()

--- js/prime.js
+++ js/prime.js
@@ -1,12 +1,16 @@
 function prime(){
+  /*
+  Outputs the number of primes less than n.
+  */
           n=document.getElementById('num').value;
-          m=parseInt(n);
-          n=Math.ceil(n);
-
-          if(isNaN(m)){
+          m=parseInt(n); // used to validate the input.
+          n=Math.ceil(n); // in case a float is given as input.
+          var MAX = 10**8;
+          // input validation
+          if(isNaN(m)){ 
             document.getElementById('resultPos').style.display="none";
           	document.getElementById('missingPos').style.display="inline-table";
-          	document.getElementById('missingPos').innerHTML='please enter a positive number';
+          	document.getElementById('missingPos').innerHTML='Please enter a positive number';
           	return 0;
           }
 
@@ -17,7 +21,16 @@ function prime(){
             document.getElementById('resultPos').style.display="inline-table";
             return 0; 
           }
-         
+
+          // if n is too large output an asymptotic approximation n/ln(n)
+          if(n>MAX){
+            document.getElementById('printResult').innerHTML='approximately '+Math.floor(n/Math.log(n));
+            document.getElementById('resultPos').style.display="inline-table";
+            return 0; 
+          }
+
+          // for n<MAX we use the Sieve of Eratosthene 
+
           var verdict = new Array(n);
           verdict[0],verdict[1]=false,false
           for(i=2;i<n;i++){
